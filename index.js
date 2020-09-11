b029310764778a49f30c345ff70761751333fc98
Ritesh Ranjan <unsortedarray@Riteshs-MBP.Dlink>
2020-09-12 00:06:50 +0530

    initial message

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+const bodyParser = require("body-parser");
+const port = 3000
+const serialiser = require('node-serialize')
+const mongodb = require('mongodb');
+
+app.use(express.urlencoded());
+
+
+// Parse JSON bodies (as sent by API clients)
+const mongoURI = "http://localhost:"+ process.env.mongoPort + "/"+ process.env.dbName;
+
+app.use(express.json());
+const connection = mongodb.MongoClient.connect(mongoURI,(err, dbClient) =>{
+    if(err){
+        console.log('connection failed')
+        res.status(503)
+        res.send('connection failed with db')
+        return
+    }
+})
+connection.connect();
+app.get('/', (req, res) => res.send('Hello World!'))
+
+// your code goes here
+
+// here
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+module.exports = app;
