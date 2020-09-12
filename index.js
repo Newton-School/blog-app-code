832d5f2589f7d7c568dd4bd829e5fbed1c8f6bc9
zhuXian2902 <2902sandeepkumar@gmail.com>
2020-09-12 23:23:38 +0530

    init commit 2

--- index.js
+++ index.js
@@ -15,80 +15,93 @@ const mongoURI = 'mongodb://localhost:27017' + '/blog';
 console.log(mongoURI);
 
 app.use(express.json());
-let col;
+// let col;
 const connection = mongodb.MongoClient.connect(mongoURI, (err, dbClient) => {
 	if (err) {
 		console.log('connection failed');
 		return;
 	}
 	const db = dbClient.db('blog');
-	col = db.collection('blog');
+	const col = db.collection('blog');
 	col.createIndex({ topic: 'text' });
-});
 
-app.get('/', (req, res) => res.send('Hello World!'));
+	app.get('/allblog', (req, res) => {
+		const page = req.query.page * 1 || 1;
+		const limit = 5;
+		const skip = (page - 1) * limit;
+		const str = req.query.search || '';
+		col
+			.aggregate([
+				// { $match: { $text: { $search: str } } },
+				{
+					$skip: skip,
+				},
+				{
+					$limit: limit,
+				},
+			])
+			.toArray()
+			.then((results) => {
+				// console.log(results);
+				res.status(200).json({
+					status: 'success',
+					result: results,
+				});
+			})
+			.catch((error) => console.error(error));
+	});
 
-// your code goes here
-
-app.get('/allblog', (req, res) => {
-	const page = req.query.page * 1 || 1;
-	const limit = 5;
-	const skip = (page - 1) * limit;
-	const str = req.query.search || '';
-	col
-		.aggregate([
-			// { $match: { $text: { $search: str } } },
-			{
-				$skip: skip,
-			},
-			{
-				$limit: limit,
-			},
-		])
-		.toArray()
-		.then((results) => {
-			// console.log(results);
-			res.status(200).json({
-				status: 'success',
-				result: results,
-			});
-		})
-		.catch((error) => console.error(error));
-});
-
-app.post('/post/blog', (req, res) => {
-	// console.log(req.body);
-	const { topic, description, posted_at, posted_by } = req.body;
-	if (!topic || !description || !posted_at || !posted_by)
-		return res.status(200).json({
-			status: 'failed',
-		});
-	col.insertOne(res.body, (err, result) => {
-		if (err) {
+	app.post('/post/blog', (req, res) => {
+		// console.log(req.body);
+		const { topic, description, posted_at, posted_by } = req.body;
+		if (!topic || !description || !posted_at || !posted_by)
 			return res.status(200).json({
 				status: 'failed',
 			});
-		}
-		const data = result.ops[0];
-		res.status(200).json({
-			status: 'success',
-			result: data,
+		col.insertOne(res.body, (err, result) => {
+			if (err) {
+				return res.status(200).json({
+					status: 'failed',
+				});
+			}
+			const data = result.ops[0];
+			res.status(200).json({
+				status: 'success',
+				result: data,
+			});
 		});
 	});
-});
 
-app.patch('/update/blog/:id', (req, res) => {
-	// console.log(req.params.id, req.body);
-	const id = new ObjectId(req.params.id);
-	col.findOneAndUpdate(
-		{ _id: id },
-		{
-			$set: req.body,
-		},
-		{
-			returnNewDocument: true,
-		},
-		(err, result) => {
+	app.patch('/update/blog/:id', (req, res) => {
+		// console.log(req.params.id, req.body);
+		const id = new ObjectId(req.params.id);
+		col.findOneAndUpdate(
+			{ _id: id },
+			{
+				$set: req.body,
+			},
+			{
+				returnNewDocument: true,
+			},
+			(err, result) => {
+				if (err) {
+					return res.status(200).json({
+						status: 'failed',
+					});
+				}
+				const data = result.value;
+				res.status(200).json({
+					status: 'success',
+					result: data,
+				});
+			}
+		);
+	});
+
+	app.delete('/delete/blog/:id', (req, res) => {
+		const id = new ObjectId(req.params.id);
+		col.findOneAndDelete({ _id: id }, (err, result) => {
+			console.log(err, result);
 			if (err) {
 				return res.status(200).json({
 					status: 'failed',
@@ -99,26 +112,13 @@ app.patch('/update/blog/:id', (req, res) => {
 				status: 'success',
 				result: data,
 			});
-		}
-	);
-});
-
-app.delete('/delete/blog/:id', (req, res) => {
-	const id = new ObjectId(req.params.id);
-	col.findOneAndDelete({ _id: id }, (err, result) => {
-		console.log(err, result);
-		if (err) {
-			return res.status(200).json({
-				status: 'failed',
-			});
-		}
-		const data = result.value;
-		res.status(200).json({
-			status: 'success',
-			result: data,
 		});
 	});
 });
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+// your code goes here
 
 // here
 
