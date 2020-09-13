c60130df80bdfa7cf21603d9bddec31c1b2c77d9
KanishkKalyani <you@example.comkanishkkalyani@gmail.com>
2020-09-13 23:04:46 +0530

    Basic Functionality Complete

--- index.js
+++ index.js
@@ -1,31 +1,55 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 const bodyParser = require("body-parser");
-const port = 3000
-const serialiser = require('node-serialize')
-const mongodb = require('mongodb');
+const port = 3000;
+const serialiser = require("node-serialize");
+const mongodb = require("mongodb");
+const morgan = require("morgan");
+const blogRoutes = require("./src/routes");
+const mongoose = require("mongoose");
 
 app.use(express.urlencoded());
 
-
 // Parse JSON bodies (as sent by API clients)
-const mongoURI = "mongodb://localhost:27017"+ "/blog"
+const mongoURI = "mongodb://localhost:27017" + "/blog";
 console.log(mongoURI);
 
 app.use(express.json());
-const connection = mongodb.MongoClient.connect(mongoURI,(err, dbClient) =>{
-    if(err){
-        console.log('connection failed')
-        return
-    }
-})
+// const connection = mongodb.MongoClient.connect(mongoURI, (err, dbClient) => {
+// 	if (err) {
+// 		console.log("connection failed");
+// 		return;
+// 	}
+// });
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get("/", (req, res) => res.send("Hello World!"));
 
 // your code goes here
 
+mongoose
+	.connect(mongoURI, {
+		useCreateIndex: true,
+		useFindAndModify: true,
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => {
+		app.listen(port, () => {
+			console.log(`Example app listening on port ${port}!`);
+		});
+	})
+	.catch(err => {
+		console.error("DB Connection Failed", err);
+	});
+
+app.use(morgan("dev"));
+
+app.use("/", blogRoutes);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
 // here
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+// app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 module.exports = app;
