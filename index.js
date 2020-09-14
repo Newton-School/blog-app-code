5b6ff6a55f35f17e5498f60ccecc20e14c55521b
UMESHRAUT <rautumesh34@gmail.com>
2020-09-14 15:04:54 +0530

    update

--- index.js
+++ index.js
@@ -1,45 +1,32 @@
-
-
-const express = require("express");
-const app = express();
+const express = require('express')
+const app = express()
 const bodyParser = require("body-parser");
-const port = 3000;
-const serialiser = require("node-serialize");
-const mongodb = require("mongodb");
-const mongoose = require("mongoose");
+const port = 3000
+const serialiser = require('node-serialize')
+const mongodb = require('mongodb');
+const mongoose=require('mongoose')
 
 app.use(express.urlencoded());
 
+
 // Parse JSON bodies (as sent by API clients)
-const mongoURI = "mongodb://localhost:27017" + "/blog";
+const mongoURI = "mongodb://localhost:27017"+"/blog"
 console.log(mongoURI);
 
 app.use(express.json());
-// const connection = mongodb.MongoClient.connect(mongoURI, (err, dbClient) => {
-//   if (err) {
-//     console.log("connection failed");
-//     return;
-//   }
-// });
+mongoose.connect(mongoURI,(err, dbClient) =>{
+    if(err){
+        console.log('connection failed')
+        return
+    }
+})
 
-mongoose
-  .connect(mongoURI, {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: true,
-  })
-  .catch((err) => {
-    console.log("connection failed");
-  });
-
-app.use("/", require('./route/api'));
-app.get("/", (req, res) => res.send("Hello World!"));
-
+// app.get('/', (req, res) => res.send('Hello World!'))
+app.use('/',require('./route/api'))
 // your code goes here
 
 // here
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 module.exports = app;
