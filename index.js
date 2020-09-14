8f5be1187ca2af4e0569695889302250155ce023
UMESHRAUT <rautumesh34@gmail.com>
2020-09-14 14:58:40 +0530

    implemented

--- index.js
+++ index.js
@@ -1,29 +1,45 @@
-const express = require('express')
-const app = express()
+
+
+const express = require("express");
+const app = express();
 const bodyParser = require("body-parser");
-const port = 3000
-const serialiser = require('node-serialize')
-const mongodb = require('mongodb').MongoClient;
-const mongoose=require('mongoose')
+const port = 3000;
+const serialiser = require("node-serialize");
+const mongodb = require("mongodb");
+const mongoose = require("mongoose");
 
 app.use(express.urlencoded());
 
-
 // Parse JSON bodies (as sent by API clients)
-const mongoURI = "mongodb://localhost:27017"+"/blog"
+const mongoURI = "mongodb://localhost:27017" + "/blog";
 console.log(mongoURI);
 
 app.use(express.json());
-mongoose.connect(mongoURI)
-    .then(()=>console.log("Mongo Db connected"))
-    .catch(err=>console.log(err))
+// const connection = mongodb.MongoClient.connect(mongoURI, (err, dbClient) => {
+//   if (err) {
+//     console.log("connection failed");
+//     return;
+//   }
+// });
 
-// app.get('/', (req, res) => res.send('Hello World!'))
-app.use('/',require('./route/api'))
+mongoose
+  .connect(mongoURI, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: true,
+  })
+  .catch((err) => {
+    console.log("connection failed");
+  });
+
+app.use("/", require('./route/api'));
+app.get("/", (req, res) => res.send("Hello World!"));
+
 // your code goes here
 
 // here
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 module.exports = app;
