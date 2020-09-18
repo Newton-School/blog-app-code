81659ab0d8dcc01217ceb9eded5a4658e285b8e5
Anushree P M <65195869+Anushreepm@users.noreply.github.com>
2020-09-18 09:57:05 +0530

    changes made

---
+++ src/schema.js
@@ -0,0 +1,26 @@
+const { model, Schema } = require("mongoose");
+
+const BlogSchema = new Schema({
+  topic: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  description: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  posted_at: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  posted_by: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+});
+
+module.exports = model("blog", BlogSchema);
