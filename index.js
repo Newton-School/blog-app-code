52c5412503bf6fd5df81f552ed090e97ef3fd99a
Bhimashankar <bhimamalbhage@hotmail.com>
2020-09-19 18:25:05 +0530

    First Commit

--- index.js
+++ index.js
@@ -1,31 +1,41 @@
-const express = require('express')
-const app = express()
-const bodyParser = require("body-parser");
-const port = 3000
-const serialiser = require('node-serialize')
-const mongodb = require('mongodb');
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+
+const mongoose = require("mongoose");
 
 app.use(express.urlencoded());
-
+app.use(express.json());
 
 // Parse JSON bodies (as sent by API clients)
-const mongoURI = "mongodb://localhost:27017"+ "/blog"
+const mongoURI = "mongodb://localhost:27017/blog-project";
+
 console.log(mongoURI);
 
-app.use(express.json());
-const connection = mongodb.MongoClient.connect(mongoURI,(err, dbClient) =>{
-    if(err){
-        console.log('connection failed')
-        return
-    }
-})
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-with, Content-Type, Accept"
+//   );
+//   res.header(
+//     "Access-Control-Allow-Methods",
+//     "Origin, X-Requested-with, Content-Type, Accept"
+//   );
+//   next();
+// });
 
-app.get('/', (req, res) => res.send('Hello World!'))
+mongoose
+  .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("mongo db connected"))
+  .catch((err) => console.log(err));
 
+// app.get("/", (req, res) => res.send("Hello World!"));
+app.use("/", require("./route/api"));
 // your code goes here
 
 // here
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 module.exports = app;
