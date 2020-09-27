5a895a110f06a3b7328587caf75dc3343e9be956
Sumit9669 <sumitsinghania68@gmail.com>
2020-09-27 14:44:16 +0530

    changes

--- index.js
+++ index.js
@@ -1,32 +1,37 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 const bodyParser = require("body-parser");
-const port = 3000
-const serialiser = require('node-serialize')
-const mongodb = require('mongodb');
-const feed = require('./routes/blog');
+const port = 3000;
+const serialiser = require("node-serialize");
+const mongodb = require("mongodb");
+const feed = require("./routes/blog");
 app.use(express.urlencoded());
 
-
 // Parse JSON bodies (as sent by API clients)
-const mongoURI = "mongodb://localhost:27017"+ "/blog"
+const mongoURI = "mongodb://localhost:27017" + "/blog";
 console.log(mongoURI);
 
 app.use(express.json());
-const connection = mongodb.MongoClient.connect(mongoURI,(err, dbClient) =>{
-    if(err){
-        console.log('connection failed')
-        return
+const connection = mongodb.MongoClient.connect(
+  mongoURI,
+  (err, dbClient) => {
+    if (err) {
+      console.log("connection failed");
+      return;
     }
-})
+  },
+  {
+    useUnifiedTopology: true
+  }
+);
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get("/", (req, res) => res.send("Hello World!"));
 app.use("/", feed);
 
 // your code goes here
 
 // here
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 module.exports = app;
