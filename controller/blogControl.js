fcbe3d67e7d1050fa3fabab1b11bcf9a11a8fbb2
Sumit9669 <sumitsinghania68@gmail.com>
2020-09-27 15:39:03 +0530

    changes

--- controller/blogControl.js
+++ controller/blogControl.js
@@ -1,59 +1,60 @@
 const Blog = require("../models/Blog");
 
-
 exports.createPost = (req, res, next) => {
-    const topic = req.body.topic;
-    const description = req.body.description;
-  
-    console.log(topic + " " + description);
-    const blog = new Blog({
-      topic: req.body.topic,
-      description: req.body.description,
-      posted_by: req.body.posted_by
-    });
-  
-    blog
-      .save()
-      .then(result => {
-        console.log(result);
-        res.status(201).json({
-          message: "post created successfully",
-          blog: result
-        });
-      })
-      .catch(err => {
-          console.log(err);
-          return res.json({
-            status: "failed"
-          });
-      });
-  };
-  
-  exports.updatePost = async (req, res, next) => {
-    try {
-      const id = req.params.id;
-      const updates = req.body;
-      const options = { new: true };
-      const result = await Blog.findByIdAndUpdate(id, updates, options);
-      res.json(result);
-    } catch (error) {
-        return res.json({
-            status: "failed"
-          });
-    }
-  };
-  exports.deletePost = async (req, res, next) => {
-    try {
-      const id = req.params.id;
-  
-      const result = await Blog.findByIdAndDelete(id);
+  const topic = req.body.topic;
+  const description = req.body.description;
+
+  console.log(topic + " " + description);
+  const blog = new Blog({
+    topic: req.body.topic,
+    description: req.body.description,
+    posted_by: req.body.posted_by
+  });
+
+  blog
+    .save()
+    .then(result => {
+      console.log(result);
       res.status(201).json({
-        message: "success"
+        message: "post created successfully",
+        blog: result
       });
-    } catch (error) {
-        return res.json({
-            status: "failed"
-          });
-    }
-  };
-  
+    })
+    .catch(err => {
+      console.log(err);
+      return res.json({
+        status: "failed"
+      });
+    });
+};
+
+exports.updatePost = async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const updates = req.body;
+    const options = { new: true };
+    const result = await Blog.findByIdAndUpdate(id, updates, options);
+    res.json({
+      status: "success",
+      result: result
+    });
+  } catch (error) {
+    return res.json({
+      status: "failed"
+    });
+  }
+};
+exports.deletePost = async (req, res, next) => {
+  try {
+    const id = req.params.id;
+
+    const result = await Blog.findByIdAndDelete(id);
+    res.status(201).json({
+      message: "success"
+    });
+  } catch (error) {
+    return res.json({
+      status: "failed"
+    });
+  }
+};
