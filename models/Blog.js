1e86d3089735f05b2d51aba948d7360f3a69a701
Sumit9669 <sumitsinghania68@gmail.com>
2020-10-05 10:10:35 +0530

    changes

--- models/Blog.js
+++ models/Blog.js
@@ -1,26 +1,14 @@
 const { model, Schema } = require("mongoose");
 
-const BlogSchema = new Schema({
-  topic: {
-    type: String,
-    trim: true,
-    required: true
+const BlogSchema = new Schema(
+  {
+    _id: { type: String, default: mongoose.Types.ObjectId },
+    topic: { type: String, required: true },
+    description: { type: String, required: true },
+    posted_at: { type: String, required: true },
+    posted_by: { type: String, required: true }
   },
-  description: {
-    type: String,
-    trim: true,
-    required: true
-  },
-  posted_at: {
-    type: String,
-    trim: true,
-    required: true
-  },
-  posted_by: {
-    type: String,
-    trim: true,
-    required: true
-  }
-});
+  { versionKey: false }
+);
 
 module.exports = model("blog", BlogSchema);
